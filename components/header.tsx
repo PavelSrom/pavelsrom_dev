22852773e16c966832794dadb730c4925371dbc8
Pavel Srom <srompavel98@gmail.com>
2022-09-18 15:03:45 +0200

    feat(header): basic navigation

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,23 @@
-import useTranslation from 'next-translate/useTranslation'
-import setLanguage from 'next-translate/setLanguage'
 import { Container } from 'ui/container'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import { useTranslation, setLanguage } from 'lib/translation'
 
 export const Header = () => {
+  const router = useRouter()
   const { t, lang } = useTranslation('common')
 
   return (
     <header className="h-16 bg-teal-100">
       <Container className="flex justify-between items-center">
-        <p>{t('hello')}</p>
+        <p onClick={() => router.push('/')}>{t('hello')}</p>
+        <nav className="space-x-6">
+          {['blog', 'about', 'experience', 'tools'].map(link => (
+            <Link href={`/${link}`} key={link}>
+              <a>{t(`navigation.${link}`)}</a>
+            </Link>
+          ))}
+        </nav>
         <button
           onClick={() =>
             setLanguage(lang === 'en' ? 'da' : lang === 'da' ? 'cs' : 'en')
