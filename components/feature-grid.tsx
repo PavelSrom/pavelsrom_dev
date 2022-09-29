2c1c72068433ec4b2b5a40da0925cab1f13c6817
Pavel Srom <srompavel98@gmail.com>
2022-09-29 23:27:45 +0200

    feat(app): basic framer motion grid animation

--- components/feature-grid.tsx
+++ components/feature-grid.tsx
@@ -1,4 +1,22 @@
+import { motion, Variants } from 'framer-motion'
+import { useState } from 'react'
 import { Container, Text } from 'ui'
+
+const variants: Variants = {
+  hidden: {
+    scale: 0,
+    y: 256,
+  },
+  visible: {
+    scale: 1,
+    y: 0,
+    transition: {
+      type: 'spring',
+      bounce: 0.4,
+      duration: 0.6,
+    },
+  },
+}
 
 export interface IconItem {
   icon: JSX.Element
@@ -11,26 +29,33 @@ interface FeatureGridProps {
   items: IconItem[]
 }
 
-export const FeatureGrid = ({ title, items }: FeatureGridProps) => (
-  <section className="flex flex-col">
-    <Container>
-      <Text variant="h1" className="mb-8">
-        {title}
-      </Text>
-      <div className="grid grid-cols-12 gap-y-8 lg:gap-x-8">
-        {items.map(({ icon, title, text }) => (
-          <div
-            key={title}
-            className="col-span-12 lg:col-span-6 bg-lightblue p-8 rounded-3xl"
-          >
-            {icon}
-            <Text variant="h2" className="mt-2">
-              {title}
-            </Text>
-            <Text className="mt-4">{text}</Text>
-          </div>
-        ))}
-      </div>
-    </Container>
-  </section>
-)
+export const FeatureGrid = ({ title, items }: FeatureGridProps) => {
+  return (
+    <section className="flex flex-col">
+      <Container>
+        <Text variant="h1" className="mb-8">
+          {title}
+        </Text>
+        <div className="grid grid-cols-12 gap-y-8 lg:gap-x-8">
+          {items.map(({ icon, title, text }, index) => (
+            <motion.div
+              key={title}
+              className="col-span-12 lg:col-span-6 bg-lightblue p-8 rounded-3xl"
+              transition={{ duration: 0.25, ease: 'easeInOut' }}
+              viewport={{ once: true, amount: 0.3 }}
+              variants={variants}
+              initial="hidden"
+              whileInView="visible"
+            >
+              {icon}
+              <Text variant="h2" className="mt-2">
+                {title}
+              </Text>
+              <Text className="mt-4">{text}</Text>
+            </motion.div>
+          ))}
+        </div>
+      </Container>
+    </section>
+  )
+}
