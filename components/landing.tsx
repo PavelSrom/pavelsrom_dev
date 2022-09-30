88d571a196191abf401a989cfa71c632136e57ad
Pavel Srom <srompavel98@gmail.com>
2022-09-30 10:20:20 +0200

    feat: homepage and about page animated

--- components/landing.tsx
+++ components/landing.tsx
@@ -1,4 +1,27 @@
+import { motion } from 'framer-motion'
 import { Container } from 'ui'
+
+const textVariants = {
+  hidden: {
+    opacity: 0,
+    x: -64,
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+  },
+}
+
+const imageVariants = {
+  hidden: {
+    opacity: 0,
+    y: 64,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+  },
+}
 
 interface LandingProps {
   illustration: string
@@ -12,13 +35,25 @@ export const Landing = ({ illustration, textContent }: LandingProps) => (
   >
     <Container className="flex items-center h-full">
       <div className="grid grid-cols-1 lg:grid-cols-12 lg:gap-16">
-        <div className="col-span-12 lg:col-span-6 order-2 lg:order-1 mt-8 lg:mt-0">
+        <motion.div
+          transition={{ duration: 0.5, ease: 'easeInOut', delay: 0.25 }}
+          viewport={{ once: true, amount: 0.3 }}
+          variants={textVariants}
+          initial="hidden"
+          whileInView="visible"
+          className="col-span-12 lg:col-span-6 order-2 lg:order-1 mt-8 lg:mt-0"
+        >
           {textContent}
-        </div>
+        </motion.div>
         <div className="col-span-12 lg:col-span-6 order-1 lg:order-2">
-          <img
+          <motion.img
             src={`/assets/${illustration}.svg`}
             alt={illustration}
+            transition={{ duration: 0.5, ease: 'easeInOut' }}
+            viewport={{ once: true, amount: 0.3 }}
+            variants={imageVariants}
+            initial="hidden"
+            whileInView="visible"
             className="max-w-full h-auto"
           />
         </div>
--- components/text-with-image.tsx
+++ components/text-with-image.tsx
@@ -1,5 +1,28 @@
 import clsx from 'clsx'
+import { motion } from 'framer-motion'
 import { Container, Text } from 'ui'
+
+const textVariants = {
+  hidden: {
+    opacity: 0,
+    y: 64,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+  },
+}
+
+const imageVariants = (textPosition: 'left' | 'right') => ({
+  hidden: {
+    opacity: 0,
+    x: textPosition === 'left' ? 64 : -64,
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+  },
+})
 
 interface TextWithImage {
   image: string
@@ -23,7 +46,12 @@ export const TextWithImage = ({
   id,
 }: TextWithImage) => {
   const textHalf = (
-    <div
+    <motion.div
+      transition={{ duration: 0.5, ease: 'easeInOut' }}
+      viewport={{ once: true, amount: 0.7 }}
+      variants={textVariants}
+      initial="hidden"
+      whileInView="visible"
       className={clsx(
         'col-span-12 lg:col-span-6 order-2 flex flex-col items-start lg:justify-center',
         {
@@ -39,17 +67,22 @@ export const TextWithImage = ({
         {text}
       </Text>
       {cta && cta}
-    </div>
+    </motion.div>
   )
 
   const imageHalf = (
-    <div
+    <motion.div
+      transition={{ duration: 0.5, ease: 'easeInOut' }}
+      viewport={{ once: true, amount: 0.7 }}
+      variants={imageVariants(textPosition)}
+      initial="hidden"
+      whileInView="visible"
       className={clsx('col-span-12 lg:col-span-6 order-1', {
         'lg:order-2': textPosition === 'left',
         'lg:order-1': textPosition === 'right',
       })}
     >
-      <img
+      <motion.img
         src={`/assets/${image}`}
         alt={imageAlt}
         className="max-w-2xl w-full h-auto rounded-3xl"
@@ -59,7 +92,7 @@ export const TextWithImage = ({
           {caption}
         </Text>
       )}
-    </div>
+    </motion.div>
   )
 
   return (
