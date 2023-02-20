8f035110719c1d35074d8056ba973d3c79e249d5
Pavel Srom <srompavel98@gmail.com>
2023-02-20 17:39:06 +0100

    feat(blog): another article

--- components/blog/blog-image.tsx
+++ components/blog/blog-image.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react'
 import { Text } from 'ui'
 
 interface BlogImageProps {
@@ -5,15 +6,29 @@ interface BlogImageProps {
   alt: string
 }
 
-export const BlogImage = ({ src, alt }: BlogImageProps) => (
-  <>
-    <img
-      src={src}
-      alt={alt}
-      className="w-auto h-auto max-w-full max-h-blog-image rounded-xl block mx-auto"
-    />
-    <Text variant="caption" className="mt-2">
-      {alt}
-    </Text>
-  </>
-)
+export const BlogImage = ({ src, alt }: BlogImageProps) => {
+  const [textWidth, setTextWidth] = useState<number>(0)
+  const imageRef = useRef<HTMLImageElement | null>(null)
+
+  useEffect(() => {
+    if (imageRef.current) {
+      setTextWidth(imageRef.current.width)
+    }
+  }, [imageRef.current?.width])
+
+  return (
+    <>
+      <img
+        src={src}
+        alt={alt}
+        ref={imageRef}
+        className="w-auto h-auto max-w-full max-h-blog-image rounded-xl block mx-auto"
+      />
+      <div style={{ maxWidth: textWidth }} className="text-left mx-auto">
+        <Text variant="caption" className="mt-2">
+          {alt}
+        </Text>
+      </div>
+    </>
+  )
+}
